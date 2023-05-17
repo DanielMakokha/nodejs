587a63dcd7996072d16b6b336097a632d5e31f26
DanielMakokha <danielmakokhan@gmail.com>
2023-05-17 13:23:14 +0300

    Minicalculator part

--- index.js
+++ index.js
@@ -2,8 +2,35 @@
 // console.log('Dtech');
 // understanding modules
 
-const {add,sub} = require('./add')
-console.log(add(3,4));
-console.log(sub(10,4));
-console.log(__dirname);
-console.log(__filename);
+// const {add,sub} = require('./add')
+// console.log(add(3,4));
+// console.log(sub(10,4));
+// console.log(__dirname);
+// console.log(__filename);
+
+const add= require('./calculator/add');
+const div= require('./calculator/div');
+const mul= require('./calculator/mul');
+const sub= require('./calculator/sub');
+
+// console.log(add(3,6));
+const a=parseInt(process.argv[2]);
+const choice=(process.argv[3]);
+const b=parseInt(process.argv[4]);
+
+// console.log(a,choice,b);
+
+if (choice === 'ADD') {
+    console.log(add
+        (a,b));
+}else if (choice === 'SUB') {
+    console.log(sub(a,b));
+}else if (choice === 'MUL') {
+    console.log(mul(a,b));
+}else if (choice === 'DIV') {
+    console.log(div(a,b));
+}else{
+    console.log('Invalid choice');
+}
+
+
