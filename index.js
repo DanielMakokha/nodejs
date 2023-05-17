19b5ed0e077d71a3e91dce0c1af4e0f14898a256
DanielMakokha <danielmakokhan@gmail.com>
2023-05-17 12:54:01 +0300

    path finding

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('Hello Daniel Makokha');
-console.log('Dtech');
+// console.log('Hello Daniel Makokha');
+// console.log('Dtech');
+// understanding modules
+
+const {add,sub} = require('./add')
+console.log(add(3,4));
+console.log(sub(10,4));
+console.log(__dirname);
+console.log(__filename);
